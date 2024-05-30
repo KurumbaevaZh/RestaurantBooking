23a35f856b7fdb949e4eb54cc83499b1319822eb
kurumbaevazania@gmail.com <kurumbaevazania@gmail.com>
2024-05-30 14:44:47 +0500

    Добавьте файлы проекта.

---
+++ RestaurantBooking/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Windows;
+using Newtonsoft.Json;
+using RestaurantBookingLibrary.Interfaces;
+using RestaurantBookingLibrary.Managers;
+using RestaurantBookingLibrary.Models;
+
+namespace RestaurantBooking
+{
+    public partial class MainWindow : Window
+    {
+        private IUserManager userManager;
+        private User currentUser;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            userManager = new UserManager();
+        }
+
+        private void LoginButton_Click(object sender, RoutedEventArgs e)
+        {
+            string username = UsernameTextBox.Text;
+            string password = PasswordBox.Password;
+            currentUser = userManager.Login(username, password);
+
+            if (currentUser != null)
+            {
+                MessageBox.Show("Вы вошли в свою учетную запись");
+                OpenReservationWindow();
+            }
+            else
+            {
+                MessageBox.Show("Неверные учетные данные");
+            }
+        }
+        //регистрация
+        private void RegisterButton_Click(object sender, RoutedEventArgs e)
+        {
+            string username = UsernameTextBox.Text;
+            string password = PasswordBox.Password;
+            var newUser = new User { Username = username, Password = password };
+            userManager.RegisterUser(newUser);
+            MessageBox.Show("Регистрация прошла успешно");
+        }
+
+        private void OpenReservationWindow()
+        {
+            var reservationWindow = new ReservationWindow(currentUser);
+            reservationWindow.Show();
+            this.Close();
+        }
+    }
+}
---
+++ RestaurantBooking/ReservationWindow.xaml.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using RestaurantBookingLibrary.Interfaces;
+using RestaurantBookingLibrary.Managers;
+using RestaurantBookingLibrary.Models;
+
+namespace RestaurantBooking
+{
+    public partial class ReservationWindow : Window
+    {
+        private User currentUser; // Текущий пользователь
+        private ITableManager tableManager; // Менеджер столов
+        private IReservationManager reservationManager; // Менеджер бронирований
+
+        public ReservationWindow(User user)
+        {
+            InitializeComponent();
+            currentUser = user;
+        }
+
+        // Загрузка окна
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            var restaurant = DataManager.LoadData();
+            tableManager = new TableManager(restaurant);
+            reservationManager = new ReservationManager(restaurant);
+
+            // Подписка на событие подтверждения бронирования
+            reservationManager.ReservationConfirmed += ReservationManager_ReservationConfirmed;
+            LoadReservations();
+        }
+
+        // Закрытие окна
+        private void Window_Closed(object sender, EventArgs e)
+        {
+            var restaurant = new Restaurant
+            {
+                Tables = tableManager.GetAllTables(),
+                Reservations = reservationManager.GetAllReservations()
+            };
+            DataManager.SaveData(restaurant);
+        }
+
+        // Поиск доступных столов
+        private void SearchButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (int.TryParse(PeopleCountTextBox.Text, out int guests) && DateDatePicker.SelectedDate.HasValue && TimeComboBox.SelectedItem != null)
+            {
+                var selectedDate = DateDatePicker.SelectedDate.Value;
+                var selectedTime = ((ComboBoxItem)TimeComboBox.SelectedItem).Content.ToString();
+                var dateTime = selectedDate.Add(TimeSpan.Parse(selectedTime));
+                var availableTables = tableManager.GetAvailableTablesByCapacityAndTime(guests, dateTime);
+
+                TablesDataGrid.ItemsSource = availableTables;
+            }
+            else
+            {
+                MessageBox.Show("Введите данные.");
+            }
+        }
+        private void BookButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (TablesDataGrid.SelectedItem != null)
+            {
+                MessageBox.Show("Бронирование успешно выполнено!");
+            }
+            else
+            {
+                MessageBox.Show("Пожалуйста, выберите стол для бронирования.");
+            }
+        }
+
+
+        // Обработка события подтверждения бронирования
+        private void ReservationManager_ReservationConfirmed(object sender, Reservation e)
+        {
+            MessageBox.Show($"Reservation confirmed for table {e.Table.TableId} at {e.ReservationTime}");
+        }
+
+        // Загрузка списка бронирований
+        private void LoadReservations()
+        {
+            var reservations = reservationManager.GetAllReservations()
+                .Where(r => r.User.Username == currentUser.Username)
+                .ToList();
+        }
+    }
+}
